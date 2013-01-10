c6f49d9549cb1e4972bc3c6b807690344ccc1d53
alct <git@loconte.be>
2013-01-10 03:56:39 +0100

    [enh] added shell script to download dependencies

---
+++ dependencies.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $(uname -m) == "x86_64" ]; then
+  BIN="64"
+  ARCH="amd64"
+else
+  BIN="32"
+  ARCH="i386"
+fi
+
+# phpwkhtmltopdf
+
+echo "phpwkhtmltopdf: downloading"
+wget -q "https://github.com/mikehaertl/phpwkhtmltopdf/tarball/master"
+wait $!
+
+if [ $? = 0 ]; then
+  echo "phpwkhtmltopdf: done"
+
+  tar -xf master
+
+  if [ $? = 0 ]; then
+    mv mikehaertl-*/WkHtmlToPdf.php pdf/ && rm -fr master mikehaertl-*/
+  else
+    echo "ERROR extracting phpwkhtmltopdf"
+  fi
+else
+  echo "ERROR downloading phpwkhtmltopdf"
+fi
+
+# wkhtmltopdf
+
+echo "wkhtmltopdf ($BIN bit): downloading"
+wget -q "https://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-$ARCH.tar.bz2"
+wait $!
+
+if [ $? = 0 ]; then
+  echo "wkhtmltopdf ($BIN bit): done"
+
+  tar -xjf *.tar.bz2
+
+  if [ $? = 0 ]; then
+    rm -f *.tar.bz2 && chmod +x wkhtmltopdf-* && mv wkhtmltopdf-* pdf/
+  else
+    echo "ERROR extracting wkhtmltopdf ($BIN bit)"
+  fi
+else
+  echo "ERROR downloading wkhtmltopdf ($BIN bit)"
+fi
